8aab10481d099408cf7f4e3dec1148f0ac5cd029
Aviv Shafir <ashafir20@gmail.com>
2014-05-28 23:14:19 +0300

    First Commit.

---
+++ NinjectIoC/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Ninject;
+using Ninject.Modules;
+
+namespace NinjectIoC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kernel = new StandardKernel(new MyModule());
+
+            var shopper = kernel.Get<Shopper>();
+
+            shopper.Charge();
+
+            Console.Read();
+        }
+    }
+
+    public class MyModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Kernel.Bind<ICreditCard>().To<MasterCard>();
+        }
+    }
+
+    public class Visa : ICreditCard
+    {
+        public string Charge()
+        {
+            return "Charging with the Visa!";
+        }
+    }
+
+    public class MasterCard : ICreditCard
+    {
+        public string Charge()
+        {
+            return "Swiping the MasterCard!";
+        }
+    }
+
+    public class Shopper
+    {
+        private readonly ICreditCard creditCard;
+
+        public Shopper(ICreditCard creditCard)
+        {
+            this.creditCard = creditCard;
+        }
+
+        public void Charge()
+        {
+            var chargeMessage = creditCard.Charge();
+            Console.WriteLine(chargeMessage);
+        }
+    }
+
+    public interface ICreditCard
+    {
+        string Charge();
+    }
+}
